33b08ad5b4f5018cb798d1fd866c7a6b8fc42dd4
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-12 21:59:54 -0800

    added card

--- src/components/InputDay.js
+++ src/components/InputDay.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 export default function InputDay({ changeDate }) {
+  
   // * * * * STORES INPUT DATE
   let input = '';
   const handleChange = (event) => {
     input = event.target.value;
+    setButtonStatus(input.length === 10);
   }
+
+  // const buttonEnabled = input.length > 0;
+
+  const [buttonStatus, setButtonStatus] = useState(false);
 
   return (
     <>
       <h2>Go to a Different Day</h2>
       <form>
-        <TextField id="standard-basic" label="Date YYYY-MM-DD" onChange={handleChange} />
-        <Button variant="contained" color="primary" onClick={event => changeDate(event, input)}>
+        <TextField id="standard-basic" label="Date YYYY-MM-DD" onChange={handleChange} /><br /><br />
+        <Button variant="contained" color="primary" onClick={event => changeDate(event, input)} disabled={!buttonStatus}>
           Change Date
         </Button>
       </form>
